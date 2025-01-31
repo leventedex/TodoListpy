9a51c0aaf8ace6393054df64b4b15eaa1cb02994
leventedex <leventedex@gmail.com>
2025-01-31 22:31:24 +0100

    d3

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-user_prompt = "Enter todo:\" "
 todos = []
 while True :
-    todo = input(user_prompt)
-    print(todo.capitalize())
-    todos.append(todo)
-    print(todos)
+    user_action = input("Type (a)dd / (s)how / e(x)it: ")
+    user_action = user_action.strip()
+    match user_action:
+        case 'add' | 'a':
+            todo=input("Enter a todo: ")
+            todos.append(todo)
+        case 'show' | 's':
+            for item in todos:
+                print(item)
+        case 'exit' | 'x':
+            break
+        case _:
+            print("This is not a valid input!")
+print("Bye!")
