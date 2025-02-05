f8fcaee4a046674e779cd1f076f28e4716dfbd1e
leventedex <leventedex@gmail.com>
2025-02-05 22:58:48 +0100

    d4 adding edit feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 todos = []
 while True :
-    user_action = input("Type (a)dd / (s)how / e(x)it: ")
+    user_action = input("Type (a)dd / (s)how / (e)dit e(x)it: ")
     user_action = user_action.strip()
     match user_action:
         case 'add' | 'a':
@@ -8,9 +8,17 @@ while True :
             todos.append(todo)
         case 'show' | 's':
             for item in todos:
-                print(item)
+                print(f"{todos.index(item)}: {item}")
         case 'exit' | 'x':
             break
+        case 'edit' | 'e':
+            index = int(input("Enter the index of the todo to edit: "))
+            if index < len(todos):
+                print(f"Editing todo: {todos[index]}")
+                new_todo = input("Enter the new todo: ")
+                todos[index] = new_todo
+            else:
+                print("Index out of range!")
         case _:
             print("This is not a valid input!")
 print("Bye!")
