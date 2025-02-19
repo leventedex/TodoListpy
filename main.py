2020abac499fb0d6236d5b32ca6c58380359c287
leventedex <leventedex@gmail.com>
2025-02-19 21:48:02 +0100

    d10: str.startswith(), try except, break continue.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if not os.path.exists(file):
 while True :
     user_action = input("Type: add / show / edit / complete / exit: ")
     user_action = user_action.strip()
-    if 'add' in user_action:
+    if user_action.startswith('add'):
         todo=user_action[4:]
         # read the items from file
         # file = open('todos.txt', 'r')
@@ -25,12 +25,12 @@ while True :
             todos = file.readlines()
 
         #add the new item to the list
-        todos.append(todo)
+        todos.append(todo + '\n')
         # overwrite the file with new vaues
         with open('todos.txt', 'w') as file:
             file.writelines(todos)
             
-    elif 'show' in user_action:
+    elif user_action.startswith('show'):
         with open('todos.txt', 'r') as file:
             todos = file.readlines()
 
@@ -47,37 +47,48 @@ while True :
             #print(f"{todos.index(item)}: {item}")
             item=item.strip('\n')
             print(f"{index}: {item.capitalize()}")
-    elif 'exit' in user_action:
-        break
-    elif 'edit'in user_action:
-        #index = int(input("Enter the index of the todo to edit: "))
-        index = int(user_action[5:])
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
-        if index < len(todos):
-            print(f"Editing todo: {todos[index].strip("\n")}")
-            new_todo = input("Enter the new todo: ")
-            todos[index] = new_todo + '\n'
-            #overwrite the file with new values
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
-        else:
-            print("Index out of range!")
-    elif 'complete' in user_action:
-        #index = int(input("Enter the index of the todo to complete: "))
-        index = int(user_action[9:])
-        #Build list from the file
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
-        if index < len(todos):
+    
+    elif user_action.startswith('exit'):
+        #break
+        exit("Goodbye!")
+    
+    elif user_action.startswith('edit'):
+        try:
+            #index = int(input("Enter the index of the todo to edit: "))
+            index = int(user_action[5:])
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+            if index < len(todos):
+                print(f"Editing todo: {todos[index].strip("\n")}")
+                new_todo = input("Enter the new todo: ")
+                todos[index] = new_todo + '\n'
+                #overwrite the file with new values
+                with open('todos.txt', 'w') as file:
+                    file.writelines(todos)
+            else:
+                print("Index out of range!")
+        except ValueError:
+            print("The 'edit' command should be followed by the number of the item.")
+            continue
+    
+    elif user_action.startswith('complete'):
+        try:
+            #index = int(input("Enter the index of the todo to complete: "))
+            index = int(user_action[9:])
+            #Build list from the file
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+    
             print(f"Completing todo: {todos[index].strip("\n")}")
             todos.pop(index)
             #overwrite the file with new values
             with open('todos.txt', 'w') as file:
                 file.writelines(todos)
-        else:
-            print("Index out of range!")
+        except IndexError:
+            print(f"Can't complete item:{str(index)}, that is not on the list. Please specify the number of the item as printed by the 'show' command.")
+            continue
+    
     else:
-        print("Invalid action!")
+        print("Didn't recognise any valid action!")
         
 print("Bye!")
